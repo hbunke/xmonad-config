31de43edfac16f8d2f3f460a9c5541fc73e80952
Hendrik Bunke <bunke@hbxt.org>
2019-03-10 12:56:19 +0100

    continous minor improvements ;-)

--- xmonad.hs
+++ xmonad.hs
@@ -3,16 +3,27 @@ import XMonad.Util.EZConfig (additionalKeys)
 --import XMonad.Hooks.DynamicLog (xmobar)
 import XMonad.Hooks.DynamicLog
 import qualified XMonad.StackSet as W
+-- import XMonad.Layout.NoFrillsDecoration
+-- import XMonad.Config.Xfce
+
+-- LAYOUTS:
+-- import XMonad.Layout.Grid
+-- import XMonad.Layout.Cross
+-- import XMonad.Layout.ThreeColumns
 
 
 main = xmonad  =<< xmobar myConfig
 
+
+-- myLayoutHook = ThreeCol
+
 myConfig = defaultConfig
+--   myConfig = xfceConfig
     {
        terminal = "roxterm"
 	  ,modMask = mod1Mask
       ,borderWidth = 3
---    ,   layoutHook = 
+--      ,layoutHook = myLayoutHook
       ,logHook = dynamicLog
       ,startupHook = myStartupHook
       ,manageHook = myManageHook
@@ -20,13 +31,17 @@ myConfig = defaultConfig
     }
 
     `additionalKeys`
-    [ ((mod1Mask, xK_F1), spawn "firefox")
-        ,((mod1Mask, xK_F2), spawn "chromium")
-        ,((mod1Mask, xK_F3), spawn "firefox --new-window https://mailbox.org")
+    [ ((mod1Mask, xK_F1), spawn "chromium")
+        ,((mod1Mask, xK_F2), spawn "firefox")
+        ,((mod1Mask, xK_F3), spawn "brave")
         ,((mod1Mask, xK_F4), kill)
         ,((mod1Mask, xK_Right), windows W.focusDown)
         ,((mod1Mask, xK_Left), windows W.focusUp)
+        ,((mod1Mask, xK_p), spawn "dmenu_run -fn 'Roboto-12'")
     ]
+
+
+
 
 -- dark material green
 myFocusedBorderColor = "#009688"
@@ -50,9 +65,10 @@ myStartupHook = do
     spawn "$HOME/.xmonad/xrandr.sh"
     spawn "$HOME/.xmonad/trayer.sh"
     --spawn "telegram-desktop"
-    spawn "feh --bg-scale ~/Dropbox/Flowerbed.jpg"
-    spawn "signal-desktop"
+    spawn "feh --bg-fill ~/Dropbox/Wallpaper/Flowerbed.jpg"
+ --   spawn "signal-desktop"
     spawn "dropbox"
     spawn "pnmixer"
 
+-- myLayoutHook = noFrillsDeco shrinkText def (layoutHook def)
 
